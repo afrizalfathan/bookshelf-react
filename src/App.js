a74135c410072fd8fadb8da099b3272a6d38d734
Afrizal Fathan <rieslinger8@gmail.com>
2022-09-28 16:46:12 +0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style/style.css"
+import { Button, InputGroup, Form } from "react-bootstrap"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <h2>Bookshelf App</h2>
+            <div className="input-container">
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Judul :</Form.Label>
+                        <Form.Control type="email" />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Penulis :</Form.Label>
+                        <Form.Control type="text" />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Tahun :</Form.Label>
+                        <Form.Control type="text" />
+                    </Form.Group>
+                    <Form.Check
+                        label="Sudah Dibaca"
+                        type="switch"
+                        id="custom-switch"
+                    />
+                    <Button
+                        variant="primary"
+                        type="submit"
+                        className="tombol-input"
+                    >
+                        Masukkan Buku
+                    </Button>
+                </Form>
+                <div className="not-read"></div>
+                <div className="done-read"></div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
